651dfc9dd72772e354ec048ae280aae7bd7a069d
c2v4 <the_pro@wp.pl>
2017-12-05 22:20:29 +0100

    Day 5 Plus

---
+++ src/main/kotlin/com/c2v4/advent17/Day5Plus.kt
@@ -0,0 +1,23 @@
+package com.c2v4.advent17
+
+fun maze2(input: String): Int {
+    val list = input.split('\n').filterNot { it.isEmpty() }.map { it.trim() }.map { it.toInt() }.toMutableList()
+    var i = 0
+    var pointer = 0
+    while (pointer < list.size) {
+        val temp = list[pointer]
+        if(list[pointer]>2){
+            list[pointer] -= 1
+        }else{
+            list[pointer] += 1
+
+        }
+        pointer += temp
+        i++
+    }
+    return i
+}
+
+fun main(args: Array<String>) {
+    print(maze2("day5.txt".asResource()))
+}
---
+++ src/test/kotlin/com/c2v4/advent17/Day5PlusSpec.kt
@@ -0,0 +1,26 @@
+package com.c2v4.advent17
+
+import org.assertj.core.api.Assertions
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object Day5PlusSpec : Spek({
+    given("Now, the jumps are even stranger: after each jump, if the offset was three or more, instead decrease it by 1. Otherwise, increase it by 1 as before." ) {
+        on("0\n" +
+                "3\n" +
+                "0\n" +
+                "1\n" +
+                "-3\n" ) {
+            it("Using this rule with the above example, the process now takes 10 steps, and the offset values after finding the exit are left as 2 3 2 3 -1.") {
+                Assertions.assertThat(maze2("0\n" +
+                        "3\n" +
+                        "0\n" +
+                        "1\n" +
+                        "-3\n")).isEqualTo(10)
+            }
+        }
+
+    }
+})
