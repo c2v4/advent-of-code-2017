294c5bd82afc8959eb21a59344cf4f9b9602df1e
c2v4 <sqrwol@gmail.com>
2017-12-06 20:46:35 +0100

    Day 6 - fixed comparator

--- src/main/kotlin/com/c2v4/advent17/Day6.kt
+++ src/main/kotlin/com/c2v4/advent17/Day6.kt
@@ -1,21 +1,24 @@
 package com.c2v4.advent17
 
-import java.util.*
+import java.util.Comparator.*
 
 fun debugger(input: String): Int {
     var newList = input.split(Regex("\\s")).filter { it.isNotEmpty() }.map { it.toInt() }
     val states = mutableSetOf<List<Int>>()
     while (!states.contains(newList)) {
         states.add(newList)
-        val templist = newList.toMutableList()
-        val maxWith = templist.mapIndexed({ index, value -> index to value }).maxWith(Comparator.comparing(
-                { t: Pair<Int, Int> -> t.second }).thenComparing(
-                { (first) -> first }))
+        val tempList = newList.toMutableList()
+        val maxWith = tempList
+                .mapIndexed({ index, value -> index to value })
+                .maxWith(comparing({ t: Pair<Int, Int> -> t.second })
+                        .thenComparing(comparing({ t: Pair<Int, Int> -> t.first })
+                                .reversed())
+                        )
         maxWith?.let {
-            templist[maxWith.first]=0
-            (0 until it.second).forEach { index -> templist[(it.first+1+index)%templist.size]+=1 }
+            tempList[maxWith.first] = 0
+            (0 until it.second).forEach { index -> tempList[(it.first + 1 + index) % tempList.size]+=1 }
         }
-        newList = templist
+        newList = tempList
     }
     return states.size
 }
