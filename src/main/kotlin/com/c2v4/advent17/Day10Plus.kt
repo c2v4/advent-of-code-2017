6d335f467d6bdeffbda02729a25a774e369423e6
c2v4 <the_pro@wp.pl>
2017-12-10 22:32:42 +0100

    Day 10 Plus

---
+++ src/main/kotlin/com/c2v4/advent17/Day10Plus.kt
@@ -0,0 +1,19 @@
+package com.c2v4.advent17
+
+
+fun hash2(input: String): Any =
+        input.toCharArray().map { it.toInt() }.let {
+            it + listOf(17, 31, 73, 47, 23)
+        }.let { list -> (0 until 64).fold(emptyList<Int>(), { acc, i -> acc + list }) }
+                .fold(HashState((0 until 256).toList()),
+                        { hashState, i ->
+                            reduceHashState(hashState, i)
+                        }
+                ).list.withIndex().groupBy { it.index / 16 }.map { it.value.map { it.value } }
+        .map { it.reduce({ acc, i -> i xor acc }) }.joinToString("") {
+            it.toString(16).padStart(2, '0')
+        }
+
+fun main(args: Array<String>) {
+    print(hash2("day10.txt".asResource()))
+}
